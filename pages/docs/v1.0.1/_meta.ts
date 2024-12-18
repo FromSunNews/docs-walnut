1d250ab6c07fbcb719a11efa854dcf1f7b9fc2fc
FSN <phuongtu590@gmail.com>
2024-12-18 17:59:09 +0000

    updated meta

---
+++ pages/docs/v1.0.1/_meta.ts
@@ -0,0 +1,22 @@
+const meta = {
+  index: {
+    title: "Getting Stated",
+  },
+  faq: {
+    title: "FAQ",
+  },
+  Architecture: {
+    title: "Architecture",
+  },
+  Consumer: {
+    title: "Cloud Workspace",
+  },
+  Provider: {
+    title: "Worker Workspace",
+  },
+  external: {
+    title: "External Links",
+  },
+};
+
+export default meta;

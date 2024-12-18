68d90938a6eb753c5e3931162c313529a329e16f
FSN <phuongtu590@gmail.com>
2024-12-18 17:46:01 +0000

    update ui

--- pages/_meta.ts
+++ pages/_meta.ts
@@ -2,22 +2,19 @@ const meta = {
   index: {
     type: "page",
     title: "Getting Started",
-    redirect: "/docs/v1.0.1"
+    redirect: "/docs/v1.0.1",
+    display: "hidden" 
   },
   Architecture: {
-    type: "page",
     title: "Architecture"
   },
   Blockchain: {
-    type: "page",
     title: "Blockchain"
   },
   Consumer: {
-    type: "page",
     title: "Cloud Workspace"
   },
   Provider: {
-    type: "page",
     title: "Worker Workspace"
   }
 };
